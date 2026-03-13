56ce340a4cfb0f4f8f9fa224db65fcfcf7e395e3
Aurel Adestira <aurelsalsaa75@gmail.com>
2026-03-13 15:22:17 +0700

    menambahkkan implementasi dengan state-based construction

---
+++ Aurel Adestira Salsabila tpmodul4_103082400041/DoorMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Aurel_Adestira_Salsabila_tpmodul4_103082400041
+{
+    internal class DoorMachine
+    {
+        enum State {Terkunci, Terbuka}
+
+        private State state;
+
+        public DoorMachine()
+        {
+            state = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (state == State.Terkunci)
+            {
+                state = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah dibuka");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (state == State.Terbuka)
+            {
+                state = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+}
--- Aurel Adestira Salsabila tpmodul4_103082400041/Program.cs
+++ Aurel Adestira Salsabila tpmodul4_103082400041/Program.cs
@@ -12,6 +12,15 @@ namespace Aurel_Adestira_Salsabila_tpmodul4_103082400041
             string kel = Console.ReadLine();
 
             Console.WriteLine("Kode Pos: " + kode.getKodePos(kel));
+
+            Console.WriteLine("\nSimulasi Door Machine:");
+
+
+            DoorMachine pintu = new DoorMachine();
+
+            pintu.BukaPintu();
+            pintu.KunciPintu();
+
         }
     }
 }
